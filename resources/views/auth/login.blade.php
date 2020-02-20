38307ed354acbbfffe4da1d346a28be9289a4dfb
Joshua Jamie <joshua@joshuas-mbp.home>
2020-02-20 20:46:39 +0000

    centering form and adding placeholders

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.main')
+
 @section('content')
 
 <div class="container">
@@ -29,7 +30,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input id="password" placeholder="Password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
