3b3902ec0463d02b6d9733843217028100e32395
Joshua Jamie <joshua@Joshuas-MBP.lan>
2020-03-13 15:26:51 +0000

    attend to tasks set

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -22,13 +22,11 @@
 
 
 	<h1 class="about">About</h1>
-	<img class="about-image" src="https://bit.ly/2W9kVp9" width="400" height="500">
-	<p class="about">Lorem ipsum dolor sit amet, vis ut dicat diceret, quo viris facete torquatos cu. Ea iriure timeam eum, mazim debet facilisi eum id. Vim ut posse dolore iisque, mei error tamquam pericula ad. Est et omittam prodesset, ea denique molestiae mea, ipsum detracto patrioque an ius.
+	<img class="about-image" src="https://bit.ly/2W9kVp9" width="200" height="400">
 
-	An mei fabellas inimicus, causae pertinax laboramus ex est, agam minim volumus pri in. His nullam molestie principes te, viris graece efficiendi an eam. Ne erat numquam detraxit mei, te vel delectus corrumpit. Dicam saperet elaboraret eos id, populo mollis ut his, ne dicant integre persequeris vel. Id vim atomorum volutpat evertitur. Mea zril tritani aliquando ei, tantas everti legimus cu mel, at atqui utamur molestie vel. Quaeque maluisset in cum, eum an alterum debitis neglegentur.</p>
-
-
-
+	<p class="about">My Name is Joshua Allman, I am 19 years of age and currently participating in an apprenticeship with a company called Purple Mountain.  This is the first application that I have built, and I hope to add to it throughout the future. It is an application for managing tasks, where users can create and complete any number of tasks as they wish. 
+	</p>	
+	
     <footer class="footer">
         <p class="creator">All rights reserved © Joshua Allman</p>
     </footer>
