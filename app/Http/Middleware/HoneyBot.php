752b65d4ece21fb8289ce82ad9c1ed8fb1cb4ee7
Josh Allman <joshallman@Joshs-iMac.lan>
2020-03-16 14:53:28 +0000

    spam bot

---
+++ app/Http/Middleware/HoneyBot.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+// require \App\Http\Middleware\HoneyBot::class
+
+use Closure;
+
+class HoneyBot
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if($request->isMethod('POST') && count($request->honey_pot) != 0 ){
+            return redirect('register');
+        }
+        return $next($request);
+    }
+}
