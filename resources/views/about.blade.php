3a546e5953f38a7f1dd128cd4183e88d574801ee
Josh Allman <joshallman@Joshs-iMac.lan>
2020-03-16 12:21:45 +0000

    sorted social icons

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -21,16 +21,23 @@
         @yield('content')
     </div>
 
-
 	<h1 class="about">About</h1>
 	<img class="about-image" src="https://bit.ly/2W9kVp9" width="400" height="400">
 
 	<p class="about">My Name is Joshua Allman, I am 19 years of age and currently participating in an apprenticeship with a company called Purple Mountain.  This is the first application that I have built, and I hope to add to it throughout the future. It is an application for managing tasks, where users can create and complete any number of tasks as they wish. 
 	</p>	
 
-	<i onclick="window.location.href = 'https://www.instagram.com/rhetoricality/';" class="fa fa-instagram">
-    <i onclick="window.location.href = 'https://twitter.com/Prepsody';" class="fa fa-twitter">
-    <i onclick="window.location.href = 'https://github.com/JoshuaAllman/';" class="fa fa-github-alt">
+<div class="social-container">
+    <a href="https://www.instagram.com/rhetoricality/">
+	   <i class="fa fa-instagram"></i>
+    </a>
+    <a href="https://twitter.com/Prepsody">
+        <i class="fa fa-twitter"></i>
+    </a>
+    <a href="https://github.com/JoshuaAllman/">
+        <i class="fa fa-github-alt"></i>
+    </a>
+</div>
         
     <footer class="footer">
         <p class="creator">All rights reserved © Joshua Allman</p>
