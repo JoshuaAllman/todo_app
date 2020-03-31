d61361fea3054dff26634520db89075792d1bf9f
Joshua Jamie <joshua@joshuas-mbp.home>
2020-03-31 21:36:18 +0100

    detsroy modal: in progress

--- resources/views/modal.blade.php
+++ resources/views/modal.blade.php
@@ -3,32 +3,46 @@
     <div class="container">
         <main class="card-body">
             <button class="modal-cross">&#x2715;</button>
-            <div class="modal-seperator">
+                <div class="modal-seperator">
                 <header class="card-header">New Task</header>
-                <form autocomplete="off" action="{{ route('tasks.create') }}" method="POST">
-                @csrf
-
-                    <div class="form-group row">
-                        <label class="col-md-4 col-form-label text-md-right">Title</label>
+                    <form autocomplete="off" action="{{ route('tasks.create') }}" method="POST">
+                        @csrf
+                        <div class="form-group row">
+                            <label class="col-md-4 col-form-label text-md-right">Title</label>
                             <input class="form-control" type="text" name="task">
-                    </div>
+                        </div>
 
-                    <div class="form-group row">
+                        <div class="form-group row">
                             <div class="form-check">
                                 <input class="toggle" type="checkbox" name="high_priority" value="1" {{ old('remember') ? 'checked' : '' }}>
-                 
-                                <label class="form-check-label" for="high_priority">
-                                    High Priority
-                                </label>
+                                <label class="form-check-label" for="high_priority">High Priority</label>
                             </div>
-                    </div>
+                        </div>
 
                         <div class="center">
-                            <button type="submit" class="btn btn-primary" name="task-button">
-                                Create
-                            </button>
+                            <button type="submit" class="btn btn-primary" name="task-button">Create</button>
                         </div>
+                    </form>
+                </div>
+                
+                <div class="delete-card">
+                    <div class="content-seperator">
+                        <h3 class="card-header">Warning!</h3>
+                        <p class="col-form-label">Are you sure you wish to delete this task?</p>
+                    </div>
+
+                @foreach($tasks as $task)
+                @if ($task->id == $loop->iteration)
+                <form action="{{ route('tasks.destroy', $task) }}" method="post">
+                    @method('DELETE')
+                    @csrf
+                    <div class="dm-buttons">
+                        <button onclick="deleteModalOpener(this, event);" class="cancel-delete">Cancel</button>
+                        <button id="{{$task->id}}" name="plswork" type="submit" class="confirm-delete">Confirm</button>
+                    </div>
                 </form>
+                @endif
+                @endforeach
             </div>
         </main>
     </div>
